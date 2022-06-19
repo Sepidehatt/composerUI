1537c1cb14c837ec97d4904cc0166fdb65726463
Sepideh Attar <sepideh.att@gmail.com>
2022-06-19 16:47:22 +0200

    format code

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -10,10 +10,17 @@ const Banner = () => {
       <Box display="flex"
         alignItems="center"
         justifyContent="space-between"
+      >
+        <Box mt='32px'
+          display="flex"
+          alignItems="center"
         >
-        <Box mt='32px' display="flex" alignItems="center">
-          <Image boxSize='80px' src={logo} alt='logo' />
-          <Text 
+          <Image
+            boxSize='80px'
+            src={logo}
+            alt='logo'
+          />
+          <Text
             display={{
               sm: 'none',
               base: 'none',
@@ -21,15 +28,29 @@ const Banner = () => {
               lg: 'block',
               xl: 'block',
               '2xl': 'block',
-            }} 
-          color='accent-default' 
-          textStyle='text-header-XL'
+            }}
+            color='accent-default'
+            textStyle='text-header-XL'
           >Composer
           </Text>
         </Box>
 
         <Box  >
-          <Button mt='32px' borderRadius='99px' ><Text padding='5px 12px' color='accent-default' textStyle='text-body-medium' >Request for free demo </Text><Image pr='10px' src={arrowV} /></Button>
+          <Button
+            mt='32px'
+            borderRadius='99px'
+          >
+            <Text padding='5px 12px'
+              color='accent-default'
+              textStyle='text-body-medium'
+            >
+              Request for free demo
+            </Text>
+            <Image
+              pr='10px'
+              src={arrowV}
+            />
+          </Button>
         </Box>
       </Box>
 
@@ -41,42 +62,42 @@ const Banner = () => {
         justifyContent="center"
         textAlign='center' >
         <Text m={{
-              sm: '0',
-              base: '0',
-              md: '0 15%',
-              lg: '0 15%',
-              xl: '0 15%',
-              '2xl': '0 15%',
-            }}   
-            color='accent-default' 
-            textStyle='text-header-4XL'
-            >
+          sm: '0',
+          base: '0',
+          md: '0 15%',
+          lg: '0 15%',
+          xl: '0 15%',
+          '2xl': '0 15%',
+        }}
+          color='accent-default'
+          textStyle='text-header-4XL'
+        >
           Deploy modern web experiences with the smartest web builder
         </Text>
-        <Text 
-        m={{
-          sm: '4% 3%',
-          base: '4% 3%',
-          md: '4% 22%',
-          lg: '4% 22%',
-          xl: '4% 22%',
-          '2xl': '4% 22%',
-        }} 
+        <Text
+          m={{
+            sm: '4% 3%',
+            base: '4% 3%',
+            md: '4% 22%',
+            lg: '4% 22%',
+            xl: '4% 22%',
+            '2xl': '4% 22%',
+          }}
           color='text-secondary'
           textStyle='text-body-display-M'
-          >
+        >
           Introducing Composer - the most advanced web and landing page builder for non-programmers that helps you to speed up your digital production processes
         </Text>
         <Button borderRadius='99px' >
-          <Text 
-          padding='12px 12px' 
-          color='accent-default' 
-          textStyle='text-body-large-medium' 
+          <Text
+            padding='12px 12px'
+            color='accent-default'
+            textStyle='text-body-large-medium'
           >
-            Request for free demo 
-            </Text>
-            <Image pr='10px' src={arrowV} />
-            </Button>
+            Request for free demo
+          </Text>
+          <Image pr='10px' src={arrowV} />
+        </Button>
       </Box>
 
     </>
